0c629e40331917fd458df0be5163d6ae3fec94bb
Konstantin Riabov <k.d.riabov@gmail.com>
2023-08-27 15:06:46 +0300

    add migrate

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -1,10 +1,11 @@
 package app
 
 import (
+	"context"
 	"errors"
 	"time"
 
-	gomigrate "github.com/golang-migrate/migrate/v4"
+	"github.com/Xacor/gophermart/pkg/postgres"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"go.uber.org/zap"
@@ -19,33 +20,104 @@ func migrate(dbURI string, l *zap.Logger) {
 	if len(dbURI) == 0 {
 		l.Fatal("migrate failed", zap.Error(errors.New("environment variable or flag not declared: DATABASE_URI")))
 	}
+	const sql = `
+		BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id serial,
+    login character varying(256)[] NOT NULL,
+    password character varying(256)[] NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.orders
+(
+    id text,
+    user_id serial,
+    status order_status NOT NULL,
+    accrual bigint NOT NULL,
+    uploaded_at timestamp with time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.withdrawals
+(
+    id serial,
+    order_id text,
+    user_id serial NOT NULL,
+    sum bigint NOT NULL DEFAULT 0,
+    processed_at timestamp with time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.balances
+(
+    id serial,
+    current bigint,
+    withdrawn bigint,
+    user_id serial,
+    PRIMARY KEY (id),
+    UNIQUE (user_id)
+);
+
+ALTER TABLE IF EXISTS public.orders
+    ADD CONSTRAINT "FK_orders_users" FOREIGN KEY (user_id)
+    REFERENCES public.users (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.withdrawals
+    ADD CONSTRAINT "FK_withdrawals_orders" FOREIGN KEY (order_id)
+    REFERENCES public.orders (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.withdrawals
+    ADD CONSTRAINT "FK_withdrawals_users" FOREIGN KEY (user_id)
+    REFERENCES public.users (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.balances
+    ADD FOREIGN KEY (user_id)
+    REFERENCES public.users (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+END;`
 	var (
 		attempts = defaultAttempts
 		err      error
-		m        *gomigrate.Migrate
+		pg       *postgres.Postgres
 	)
 
 	for attempts > 0 {
-		m, err = gomigrate.New("file://migrations/migration.sql", dbURI)
+		pg, err = postgres.New(dbURI)
+		if err == nil {
+			l.Error("migrate connection failed", zap.Error(err), zap.Int("postgres is trying to connect, attempts left", attempts))
+			continue
+		}
+
+		_, err = pg.Pool.Exec(context.Background(), sql)
 		if err == nil {
 			break
 		}
+		l.Error("migration failed", zap.Error(err))
 
-		l.Error("migrate connection failed", zap.Error(err), zap.Int("postgres is trying to connect, attempts left", attempts))
 		time.Sleep(defaultTimeout)
 		attempts--
 	}
 
-	err = m.Up()
-	defer m.Close()
-	if err != nil && !errors.Is(err, gomigrate.ErrNoChange) {
-		l.Fatal("migrate up failed", zap.Error(err))
-	}
-
-	if errors.Is(err, gomigrate.ErrNoChange) {
-		l.Error("migrate", zap.Error(err))
-		return
-	}
+	defer pg.Close()
 
 	l.Info("migrate: up success")
 }
