34d2ddf6b969b468aef4c9568a52c7d898ef0ad9
Konstantin Riabov <k.d.riabov@gmail.com>
2023-08-27 14:56:37 +0300

    add auth logs

--- internal/controller/http/api/authentication.go
+++ internal/controller/http/api/authentication.go
@@ -35,22 +35,26 @@ func (ar *authRoutes) Register(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if _, err := buf.ReadFrom(r.Body); err != nil {
+		ar.l.Error("can not read body error", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if err := json.Unmarshal(buf.Bytes(), &user); err != nil {
+		ar.l.Error("can not unmarshal body", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	err := ar.a.Register(r.Context(), user)
 	if errors.Is(err, usecase.ErrUserExists) {
+		ar.l.Error("can not register user", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusConflict)
 		return
 	}
 	token, err := ar.a.Authenticate(r.Context(), user)
 	if err != nil {
+		ar.l.Error("can not authenticate user", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -66,20 +70,24 @@ func (ar *authRoutes) Authenticate(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if _, err := buf.ReadFrom(r.Body); err != nil {
+		ar.l.Error("can not read body error", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if err := json.Unmarshal(buf.Bytes(), &user); err != nil {
+		ar.l.Error("can not unmarshal body", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	token, err := ar.a.Authenticate(r.Context(), user)
 	if errors.Is(err, usecase.ErrInvalidCredentials) {
+		ar.l.Error("can not authenticate user", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	} else if err != nil {
+		ar.l.Error("can not authenticate user", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
